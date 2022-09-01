1050d96d24fb109ceb954206a3cef7acf0bc8b33
“aliciaVillon“ <villonalicia@gmail.com>
2022-09-01 03:09:25 -0500

    fin seccion . - diseño

---
+++ src/app/shared/haeder/haeder.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-haeder',
+  templateUrl: './haeder.component.html',
+  styles: [
+  ]
+})
+export class HaederComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
