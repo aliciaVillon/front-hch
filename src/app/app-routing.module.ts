29c7bdce9fce617be7d94831c5d17ccd474f8851
“aliciaVillon“ <villonalicia@gmail.com>
2023-01-17 11:51:17 -0500

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,15 @@
 import { NgModule } from '@angular/core';
+
+
+import { PagesRoutingModule } from './pages/pages.routing'; 
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component'; 
+import { AuthRoutingModule } from './auth/auth.routing';
 
-const routes: Routes = [
-  {
-    path: '', component:PagesComponent,
-    children:[ 
-      {path: '', redirectTo:'/dashboard',pathMatch: 'full'},
-      {path: 'dashboard', component:DashboardComponent},
-      {path: 'progress', component:ProgressComponent},
-      {path: 'grafica1', component:Grafica1Component},
-    ]
-  },
-  
-  {path: 'register', component:RegisterComponent},
-  {path: 'login', component:LoginComponent}, 
-  
-  
+const routes: Routes = [ 
+  {path: '', redirectTo:'/dashboard',pathMatch: 'full'}, 
   {path: '**', component:NopagefoundComponent}
 ]
 
@@ -30,7 +17,9 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [RouterModule]
 })
