441541ecbdcfd123a7c5dc939e473c25e0c46e90
“aliciaVillon“ <villonalicia@gmail.com>
2023-02-21 09:56:54 -0500

    Fin seccion 6

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType, Color } from 'chart.js';
+import { ChartData, ChartEvent } from 'chart.js';
+ 
+import { baseColors } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
   styleUrls: ['./grafica1.component.css']
 })
-export class Grafica1Component implements OnInit {
+export class Grafica1Component  {
 
-  constructor() { }
+public labels1: string[] = [ 'GALLETA', 'GASEOSA', 'CHCOCOALTE' ];
 
-  ngOnInit(): void {
-  }
-
+public data1: ChartData<'doughnut'> = {
+  labels: this.labels1,
+  datasets: [
+    { data: [ 10, 20, 45 ] }
+  ]
+};
+ 
 }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HaederComponent } from './haeder/haeder.component';
+
+
+
+@NgModule({
+  declarations: [
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HaederComponent,
+  ],
+  exports: [
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HaederComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
